b22fab390924a3383528131305b2824334f4d6f9
Trevnh <trevorhuynh@hotmail.com>
2026-05-08 10:57:28 +0000

    Add 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    if (len(argv) - 1) == 1:
+        print("{} argument:".format(len(argv) - 1))
+    else:
+       print("{} arguments:".format(len(argv) - 1))
+    for arg in range(1, len(argv)):
+       print("{}: {}".format(arg, argv[arg]))
