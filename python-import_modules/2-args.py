83ab1df9bfa829f9d6d0adeb1f9efc982841956d
Trevnh <trevorhuynh@hotmail.com>
2026-05-08 11:00:14 +0000

    Fix indentation issue for pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif len(argv) - 1 == 0:
         print("{} arguments.".format(len(argv) - 1))
     else:
-       print("{} arguments:".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
     for arg in range(1, len(argv)):
-       print("{}: {}".format(arg, argv[arg]))
+        print("{}: {}".format(arg, argv[arg]))
