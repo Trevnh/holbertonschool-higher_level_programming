bf782362c9cd9718e01659085bb781b1cbac0e6b
Trevnh <trevorhuynh@hotmail.com>
2026-05-14 01:53:16 +0000

    Fix missing map usage

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = list(my_list)
-    for item in range(0, len(new_list)):
-        new_list[item] *= number
+    new_list = list(map(lambda x: x * number, my_list))
     return new_list
