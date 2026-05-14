bfacfe3a899cbffde0207e4272b8e47a6d7129a7
Trevnh <trevorhuynh@hotmail.com>
2026-05-14 01:44:36 +0000

    Add 10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best = 0
+    for key in a_dictionary:
+        if a_dictionary[key] > best:
+            best = a_dictionary[key]
+            best_scorer = key
+    return best_scorer
