0d8b71556d1166db924ab987c54d03b62bb56829
Trevnh <trevorhuynh@hotmail.com>
2026-05-17 02:41:36 +0000

    Add 2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    length = 0
+    skip = 0
+    while length < x:
+        try:
+            print("{:d}".format(my_list[length]), end="")
+            length += 1
+        except (TypeError, ValueError):
+            length += 1
+            skip += 1
+            pass
+    print()
+    return(length - skip)
