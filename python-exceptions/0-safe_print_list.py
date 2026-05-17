a9b57788b407f6d5527f35027488c1cfe6ebf34a
Trevnh <trevorhuynh@hotmail.com>
2026-05-17 02:16:18 +0000

    Add README.md and 0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    length = 0
+    while length < x:
+        try:
+            print("{}".format(my_list[length]), end="")
+            length += 1
+        except:
+            break
+    print()
+    return length
