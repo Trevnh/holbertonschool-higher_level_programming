1e01ca3a56852dda12153cac9007e79b7bb3d94b
Trevnh <trevorhuynh@hotmail.com>
2026-05-17 02:22:05 +0000

    Revert "Change break to pass"
    This reverts commit 5a083a9ba7b2af5e0e870cc3f57c534a7d024771.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[length]), end="")
             length += 1
         except:
-            pass
+            break
     print()
     return length
