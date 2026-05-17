15ac8ec71fcf4e975bc5fe9e1e34058330017184
Trevnh <trevorhuynh@hotmail.com>
2026-05-17 04:28:39 +0000

    Add README.md and 0-add_integer.py and 0-add_integer.txt

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module to add two integers"""
+
+
+def add_integer(a, b=98):
+    """Function to add integers"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
