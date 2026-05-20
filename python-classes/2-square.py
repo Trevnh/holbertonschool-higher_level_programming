efb55f35bc52d823437c81cff67a0e690647d17c
Trevnh <trevorhuynh@hotmail.com>
2026-05-20 06:37:24 +0000

    Add 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to contain Square class"""
+
+
+class Square():
+    """Square Class"""
+    def __init__(self, size=0):
+        """Initialize a Sqaure with a size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
