cc71f41d004ab3b420661837b9c65d6494d7c3aa
Trevnh <trevorhuynh@hotmail.com>
2026-05-20 06:25:54 +0000

    Add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to contain Square class"""
+
+
+class Square():
+    """Square Class"""
+    def __init__(self, size):
+        """Initialize a Square with given size"""
+        self.__size = size
