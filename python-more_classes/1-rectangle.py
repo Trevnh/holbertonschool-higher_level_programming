bd020dd63c4d8043f5a412e5c9799dcde851d237
Trevnh <trevorhuynh@hotmail.com>
2026-05-20 11:24:00 +0000

    Add 1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module to contain Rectangle class"""
+
+
+class Rectangle():
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle with height and width"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets private instance width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Gets private instance height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Sets width to value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets height to value"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
