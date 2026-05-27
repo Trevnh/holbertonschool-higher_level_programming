8db743a320d50687af2dd02f1fb284d81a07a9bc
Trevnh <trevorhuynh@hotmail.com>
2026-05-27 09:47:30 +0000

    Add 3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function that returns True
+    if obj is an instance or inherited from a_class
+    """
+    return True if isinstance(obj, a_class) else False
