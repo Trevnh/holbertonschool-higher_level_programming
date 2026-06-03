012c504381f27496365ba16d9e72d45560bd16b9
Trevnh <trevorhuynh@hotmail.com>
2026-06-03 10:05:40 +0000

    Add README.md and 0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for Reading File"""
+
+
+def read_file(filename=""):
+    """Function for reading file"""
+    with open(filename) as file:
+        content = file.read()
+        print(content)
