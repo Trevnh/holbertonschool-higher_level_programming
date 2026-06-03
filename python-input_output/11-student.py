25667d3ba777e0f92b2c2a936bed61e580c4561f
Trevnh <trevorhuynh@hotmail.com>
2026-06-03 11:32:24 +0000

    Add 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module to hold Student class"""
+
+
+class Student():
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """Initialisation function for Student with name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of a Student"""
+        if type(attrs) is list:
+            new_dict = {key: self.__dict__[key] for key
+                        in attrs
+                        if key in self.__dict__}
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
