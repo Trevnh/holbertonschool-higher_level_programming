a1cd28379a1fa115bcf6f4c254a4e332766a7d4d
Trevnh <trevorhuynh@hotmail.com>
2026-06-07 08:09:27 +0000

    Add 12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for Pascal Triangle"""
+
+
+def pascal_triangle(n):
+    """Function to return a list of lists representing a Pascal Triangle"""
+    p_tri = []
+    if n <= 0:
+        return p_tri
+    for i in range(n):
+        current = [1]
+        for j in range(1, i):
+                current.append(p_tri[i - 1][j - 1] + p_tri[i - 1][j])
+        if i != 0:
+            current.append(1)
+        p_tri.append(current)
+    return p_tri
