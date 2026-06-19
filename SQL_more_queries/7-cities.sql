f4c08905dc8a085a6e953b2b7d91dfeddaeaed91
Trevnh <trevorhuynh@hotmail.com>
2026-06-19 12:27:54 +0000

    Add 7-cities.sql

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Create cities table onto hbtn_0d_usa database on MySQL server
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
