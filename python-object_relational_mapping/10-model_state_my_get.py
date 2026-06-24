c39dce6356086379242f892746f4164c256d0831
Trevnh <trevorhuynh@hotmail.com>
2026-06-24 11:27:17 +0000

    Add 10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module to fetch all states"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}"
+        f"@localhost:3306/{sys.argv[3]}",
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    get = f"%{sys.argv[4]}%"
+    state = session.query(State).filter(State.name.like(get)).first()
+    if state is not None:
+        print(f"{state.id}")
+    else:
+        print("Not Found")
