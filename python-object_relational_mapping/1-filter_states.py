d4fe1ccc67c3fc51c7f0187b4d63e55152373e29
Trevnh <trevorhuynh@hotmail.com>
2026-06-24 02:41:04 +0000

    Try to make sql query case sensitive

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute(
         "SELECT * "
-        "FROM states WHERE name LIKE 'N%' "
+        "FROM states WHERE name LIKE BINARY 'N%' "
         "ORDER BY id ASC"
     )
     query_rows = cur.fetchall()
