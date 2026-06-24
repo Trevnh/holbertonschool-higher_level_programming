d3b83928d4afe056b009f934468e174e4f6cc0fc
Trevnh <trevorhuynh@hotmail.com>
2026-06-24 02:18:49 +0000

    Fix pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module for Filtering states that start with N"""
 
-import MySQLdb, sys
+import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
@@ -14,7 +15,11 @@ if __name__ == "__main__":
         charset="utf8"
     )
     cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT id, name"
+        " FROM states WHERE name LIKE 'N%' "
+        "ORDER BY id ASC"
+    )
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
