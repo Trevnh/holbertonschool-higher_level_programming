63819366df7a6fe16874e1695b515c7a1f7a7293
Trevnh <trevorhuynh@hotmail.com>
2026-06-24 04:22:09 +0000

    Add 3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module for Filtering states that start with N"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT *
+        FROM states
+        WHERE BINARY name=%(state)s
+        ORDER BY id ASC
+        """, {
+            'state': sys.argv[4]
+        })
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
