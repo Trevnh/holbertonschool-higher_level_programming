4cbbd6b3f360c24eba0e4ab5807025a9e9cbc1a9
Trevnh <trevorhuynh@hotmail.com>
2026-07-12 10:46:11 +0000

    Add 11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+let biggest = -Infinity;
+let secondBiggest = -Infinity;
+
+for (let i = 2; i < process.argv.length; i++) {
+  let current = parseInt(process.argv[i]);
+
+  if (current > biggest) {
+    secondBiggest = biggest;
+    biggest = current;
+  } else if (current > secondBiggest && current !== biggest) {
+    secondBiggest = current;
+  }
+}
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  console.log(secondBiggest);
+}
